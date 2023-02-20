b3090b1899cbf7a6ebc3c6c124471dc6f00e7ec9
= <=>
2023-02-20 00:24:48 +0000

    added script to delete a vpc

--- remove_vpc.py
+++ remove_vpc.py
@@ -1,4 +1,18 @@
 #!/usr/bin/env python3.7
 
+
+# How to remove your AWS vpc
+
+# Can't delete vpc if there are other resources and gateways attached:
+# like ec2 instances, route tables(except default), and
+# security groups(except default)
+
+
 import boto3
 
+client = boto3.client("ec2")
+
+response = client.delete_vpc(
+    VpcId = 'vpc-098b28ba93af7f4c4'
+    )
+print(response)
