a5f3e9fd43a4e166fbea8b1b0fdaa11c2f03567a
= <=>
2023-02-20 06:23:15 +0000

    added script to list all ec2 instance Ids

--- get_ec2_instanceIds.py
+++ get_ec2_instanceIds.py
@@ -1,3 +1,16 @@
 #!/usr/bin/env python 3.7
 
+### How to get all ec2 instance Id's
+
 import boto3
+
+ec2 = boto3.client("ec2")
+
+response = ec2.describe_instances()
+
+for each in response['Reservations']:
+    for instance in each['Instances']:
+        print(instance['InstanceId'])
+    
+
+
